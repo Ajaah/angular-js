3df0cc94758c31bd6790d33386bbd866b47018a9
Ajah <sharonajah7@gmail.com>
2020-12-04 14:32:27 +0300

    many changes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,46 @@
-var myStarApp=angular.module('myStarApp', []);
+var myStarApp=angular.module('myStarApp', ['ngRoute', 'ngAnimate']);
 
-myStarApp.controller('StarController', ['$scope', function($scope){
+myStarApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/home',{
+        templateUrl:'views/home.html',
+        controller:'StarController'
+    })
+    .when('/dire',{
+        templateUrl:'views/dire.html',
+        controller:'StarController'
+    })
+    .when('/contact',{
+        templateUrl:'views/contact.html',
+        controller:'ContactController'
+    })
+    .when('/send-form',{
+        templateUrl:'views/send-form.html',
+        controller:'ContactController'
+    })
+    .otherwise({
+        redirectTo:'/home'
+    })
+}]);
+
+myStarApp.directive('randomStar', [function(){
+    return {
+        restrict:'E',
+        scope:{
+            title:'=',
+            stars:'='
+        },
+        templateUrl:'views/random.html',
+         transclude: true,
+         replace:true,
+        controller: function($scope){
+            $scope.random=Math.floor(Math.random() * 3);
+        }
+        
+    };
+}]);
+
+myStarApp.controller('StarController', ['$scope','$http', function($scope, $http){
 
     $scope.removeStar=function(star){
         var removedStar=$scope.stars.indexOf(star);
@@ -19,49 +59,43 @@ myStarApp.controller('StarController', ['$scope', function($scope){
         $scope.newstar.worth="";
     };
 
+    $scope.deleteAll=function(){
+        $scope.stars=[];
+    };
+
     $scope.stars=[
-        {name:"Alma",
-        band:"Pink",
-        worth:100000,
-        available:true,
-        pic: "content\images\alma.jpg"
+        {
+            name:"Sharon",
+            band:"Red",
+            worth:200000,
+            available:true,
+            pic:"content/images/shirlyne.jpg"
         },
-        {name:"Shirlyne",
-         band:"Purple",
-         worth:200000,
-         available:true,
-         pic: "content\images\shirlyne.jpg"
-         },
-         {name:"Watson",
-         band:"Green",
-         worth:300000,
-         available:true,
-         pic: "content\images\watson.jpg"
-         },
-         {name:"Ryan",
-         band:"Yellow",
-         worth:400000,
-         available:true,
-         pic: "content\images\rian.jpg"
-         },
-         {name:"Adrian",
-          band:"Blue",
-          worth:500000,
-          available:true,
-          pic: "content\images\adrian.jpg"
-         },
-         {name:"Seth",
-         band:"Red",
-         worth:600000,
-         available:true,
-         pic: "content\images\seth.jpg"
-         },
-         {name:"Joshua",
-         band:"Black",
-         worth:700000,
-         available:true,
-         pic: "content\images\joshua.jpg"
-         }
+        {
+            name:"Adrian",
+            band:"Blue",
+            worth:500000,
+            available:true,
+            pic:"content/images/adrian.jpg"
+        },
+        {
+            name:"Joshua",
+            band:"Green",
+            worth:700000,
+            available:true,
+            pic:"content/images/joshua.jpg"
+        }
     ];
 
+    // This code links stars.json to this controller
+    //$http.get('data/stars.json').success(function(data){
+     //   $scope.stars=data;
+   // });
+
+}]);
+
+myStarApp.controller('ContactController', ['$scope', '$location', function($scope, $location){
+    $scope.sendMsg=function(){
+        $location.path('/send-form');
+    };
 }]);
